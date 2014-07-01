7d003e2384ac8aa41751fcea368a69a650c53bd1
Glenn Block <glenn.block@gmail.com>
2014-07-01 13:54:25 -0700

    Refactoring to match our C# SDK layout / changing to use xUnit for tests

---
+++ test/unit-tests/TestUdpTraceListener.cs
@@ -0,0 +1,46 @@
+﻿using Splunk.Logging;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace Splunk.Logging
+{
+    public class TestUdpTraceListener
+    {
+        [Fact]
+        public void TraceToOpenUdpSocketWorks()
+        {
+            var sb = new StringBuilder();
+            var receivingUdpClient = new UdpClient(0);
+            int port = ((IPEndPoint)receivingUdpClient.Client.LocalEndPoint).Port;
+
+            var receiver = new Thread((object r) =>
+            {
+                var endpoint = new IPEndPoint(IPAddress.Loopback, port);
+                var receivedBytes = receivingUdpClient.Receive(ref endpoint);
+                ((StringBuilder)r).Append(Encoding.UTF8.GetString(receivedBytes));
+            });
+            receiver.Start(sb);
+
+            var sender = new Thread(() =>
+            {
+                var traceSource = new TraceSource("UnitTestLogger");
+                traceSource.Listeners.Remove("Default");
+                traceSource.Switch.Level = SourceLevels.All;
+                traceSource.Listeners.Add(new UdpTraceListener(IPAddress.Loopback, port));
+                traceSource.TraceEvent(TraceEventType.Information, 100, "Boris");
+            });
+            sender.Start();
+
+            receiver.Join();
+            sender.Join();
+
+            var received = sb.ToString();
+            
+            Assert.True(sb.ToString().EndsWith("UnitTestLogger Information: 100 : Boris\r\n"));
+        }
+    }
+}
