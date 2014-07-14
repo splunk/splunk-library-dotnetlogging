477a9bc8fc88177c3bb7707a4a87bca23bb0352f
Fred Ross <fross@splunk.com>
2014-07-14 10:32:54 -0700

    Update SLAB package and dependency. Fill in assembly metadata.

--- src/Splunk.Logging/Properties/AssemblyInfo.cs
+++ src/Splunk.Logging/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Splunk.Logging")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Listeners and sinks to log to Splunk from .NET logging frameworks")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Splunk, Inc.")]
 [assembly: AssemblyProduct("Splunk.Logging")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
--- test/unit-tests/TestUdpEventSink.cs
+++ test/unit-tests/TestUdpEventSink.cs
@@ -20,16 +20,10 @@ namespace Splunk.Logging
         {
             public class Keywords
             {
-                public const EventKeywords Page = (EventKeywords)1;
-                public const EventKeywords DataBase = (EventKeywords)2;
-                public const EventKeywords Diagnostic = (EventKeywords)4;
-                public const EventKeywords Perf = (EventKeywords)8;
             }
 
             public class Tasks
             {
-                public const EventTask Page = (EventTask)1;
-                public const EventTask DBQuery = (EventTask)2;
             }
 
             [Event(1, Message = "{1} - {0}", Level = EventLevel.Error)]
@@ -54,7 +48,7 @@ namespace Splunk.Logging
             });
             receiver.Start(sb);
 
-            var sender = new Thread(() =>
+            ThreadStart f = () =>
             {
                 var listener = new ObservableEventListener();
                 listener.Subscribe(new UdpEventSink(IPAddress.Loopback, port));
@@ -62,7 +56,8 @@ namespace Splunk.Logging
                 listener.EnableEvents(source, EventLevel.LogAlways, Keywords.All);
                 source.Message("Boris", "Meep");
 
-            });
+            };
+            var sender = new Thread(f);
             sender.Start();
 
             receiver.Join();
