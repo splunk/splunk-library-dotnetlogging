d60ccf1574a6ccaef3e3588c70a2c102367c2405
Fred Ross <fross@splunk.com>
2014-07-30 10:41:34 -0700

    DVPL-5297 | Split .NET logging into two packages, one for TraceListener, one for SLAB, so we don't force SLAB dependency on TraceListener users.

--- src/Splunk.Logging.SLAB/Properties/AssemblyInfo.cs
+++ src/Splunk.Logging.SLAB/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Splunk.Logging")]
-[assembly: AssemblyDescription("Listeners and sinks to log to Splunk from .NET logging frameworks")]
+[assembly: AssemblyTitle("Splunk.Logging.SLAB")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Splunk, Inc.")]
-[assembly: AssemblyProduct("Splunk.Logging")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Splunk.Logging.SLAB")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e505878-b243-4263-b2cb-207b98be34e0")]
+[assembly: Guid("706839a0-480c-41ec-a486-4c7ab8b38a55")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
