ad47912acfed3fd1cdcb25abeb1847834b146106
Fred Ross <fross@splunk.com>
2014-09-02 19:04:40 -0700

    DVPL-5298 | Fixed HostnameToIPAddress method to return the first address if multiple ones are found.
    Looking up "localhost" on Windows machines typically returns at least four addresses per network interface
    (loopback and external, in both IPv4 and IPv6). This will be even more generally true for IPv6.

--- src/Splunk.Logging.Common/Util.cs
+++ src/Splunk.Logging.Common/Util.cs
@@ -11,9 +11,6 @@ namespace Splunk.Logging
             IPHostEntry addresses = Dns.GetHostEntry(hostname);
             if (addresses.AddressList.Count() < 1)
                 throw new Exception(string.Format("No IP address corresponding to hostname {0} found.", hostname));
-            else if (addresses.AddressList.Count() > 1)
-                throw new Exception(string.Format("Found {0} IP addresses matching hostname {1}.",
-                    addresses.AddressList.Count(), hostname));
             else
                 return addresses.AddressList[0];
         }
