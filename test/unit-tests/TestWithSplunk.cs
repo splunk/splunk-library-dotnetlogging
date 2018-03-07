279280951649ae783358e7a6972eec2e5a48a18e
shilpabijam <shilpab@outlook.com>
2018-03-06 16:10:21 -0800

    get stack trace for error

--- test/unit-tests/TestWithSplunk.cs
+++ test/unit-tests/TestWithSplunk.cs
@@ -345,8 +345,8 @@ namespace Splunk.Logging
             List<HttpWebRequest> requests = new List<HttpWebRequest>();
             List<Stream> streams = new List<Stream>();
             byte[] byteArray = Encoding.UTF8.GetBytes(postData);
-            for (int i = 0; i < 100; i++)
-            {
+            for (int i = 0; i < 5; i++)
+            { 
                 var request = WebRequest.Create(baseUrl + "/services/collector") as HttpWebRequest;
                 requests.Add(request);
                 request.Timeout = 60 * 1000;
@@ -355,9 +355,16 @@ namespace Splunk.Logging
                 request.Headers.Add("Authorization", "Splunk " + token);
                 request.ContentType = "application/x-www-form-urlencoded";
                 request.ContentLength = byteArray.Length * 2;
-                Stream dataStream = request.GetRequestStream();
-                streams.Add(dataStream);
-                dataStream.Write(byteArray, 0, byteArray.Length);
+                try
+                {
+                    Stream dataStream = request.GetRequestStream();
+                    streams.Add(dataStream);
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
+                catch (Exception err) 
+                {
+                    Console.WriteLine("error os " + err.ToString());
+                }
             }
         }
         [Trait("functional-tests", "SendEventsBatchedByTime")]
