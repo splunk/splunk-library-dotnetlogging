d328b66220ee1079f34f096ebe0059a4810c10bd
Graeme Foster <graemefoster@microsoft.com>
2023-04-03 15:36:40 +0800

    Added some more messages to I can benchmark

--- standalone-test/Program.cs
+++ standalone-test/Program.cs
@@ -9,13 +9,13 @@ namespace standalone_test
     /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            DoIt();
-            Console.ReadLine();
+            await DoIt();
+            Console.WriteLine("Done");
         }
 
-        static async void DoIt()
+        static async Task DoIt()
         {
             ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) =>
             {
@@ -25,10 +25,23 @@ namespace standalone_test
             var middleware = new HttpEventCollectorResendMiddleware(100);
             var ecSender = new HttpEventCollectorSender(new Uri("https://localhost:8088"), "92A93306-354C-46A5-9790-055C688EB0C4", null, HttpEventCollectorSender.SendMode.Sequential, 5000, 0, 0, middleware.Plugin);
             ecSender.OnError += o => Console.WriteLine(o.Message);
-            ecSender.Send(DateTime.UtcNow.AddDays(-1), Guid.NewGuid().ToString(), "INFO", null, 
-                new { Foo = "Bar", test2 = "Testit2", time = ConvertToEpoch(DateTime.UtcNow.AddHours(-2)).ToString(), anotherkey="anothervalue" });
-            ecSender.Send(Guid.NewGuid().ToString(), "INFO", null,
-                new { Foo = "Bar", test2 = "Testit2", time = ConvertToEpoch(DateTime.UtcNow.AddHours(-2)).ToString(), anotherkey = "anothervalue!!" });
+            
+            for (var i = 0; i < 50000; i++)
+            {
+                ecSender.Send(DateTime.UtcNow.AddDays(-1), Guid.NewGuid().ToString(), "INFO", null,
+                    new
+                    {
+                        Foo = "Bar", test2 = "Testit2", time = ConvertToEpoch(DateTime.UtcNow.AddHours(-2)).ToString(),
+                        anotherkey = "anothervalue"
+                    });
+                ecSender.Send(Guid.NewGuid().ToString(), "INFO", null,
+                    new
+                    {
+                        Foo = "Bar", test2 = "Testit2", time = ConvertToEpoch(DateTime.UtcNow.AddHours(-2)).ToString(),
+                        anotherkey = "anothervalue!!"
+                    });
+            }
+
             await ecSender.FlushAsync();
         }
 
